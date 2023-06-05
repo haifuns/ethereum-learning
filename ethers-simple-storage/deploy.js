2df9627796e3ac9e0385ac751efc9486d27c3bce
haifuns <haifuns@163.com>
2023-06-05 20:16:45 +0800

    本地环境搭建

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,7 +1,17 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
-    console.log("hi");
-    let variable = 5;
-    console.log(variable);
+    // 连接本地区块链
+    const provider = new ethers.JsonRpcProvider("http://0.0.0.0:8545");
+    // 钱包
+    const wallet = new ethers.Wallet("", provider);
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+    const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    console.log(contract);
 }
 
 main()
---
+++ ethers-simple-storage/readme.md
@@ -0,0 +1,21 @@
+- [solcjs Solidity编译器](https://github.com/ethereum/solc-js)
+- [hardhat 以太坊开发环境](https://hardhat.org/)
+- [ganache 本地虚拟区块链](https://trufflesuite.com/ganache/)
+- [ethers.js 区块链工具包](https://ethers.org/)
+- [web3.js 区块链工具包](https://docs.web3js.org/)
+
+```
+npm i -g corepack
+corepack enable // enable yarn
+
+// yarn 镜像
+npm install -g yrm
+yrm ls
+yrm use taobao
+yrm test taobao
+
+yarn add solc
+yarn add ethers
+
+yarn add fs-extra // 文件操作工具包
+``` 
